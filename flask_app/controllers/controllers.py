414347e7a53ffd0010f933a1f65b0fd99ad85fd4
you name here <achqc1587@gmail.com>
2024-10-16 16:27:28 -0700

    updated templates

--- flask_app/controllers/controllers.py
+++ flask_app/controllers/controllers.py
@@ -4,17 +4,41 @@ from flask_app.models.model import Email
 
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
+@app.route('/process/login', methods=['post'])
+def login():
+    return redirect ('/dashboard')
+
 @app.route('/shop')
-def store():
+def shop():
     return render_template('shop.html')
 
 @app.route('/select')
 def create():
     return render_template('class_selection.html')
 
+# ---- Dashboard ----
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
+# ---- Account ----
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
+@app.route('/account/update', methods=['post'])
+def account_update():
+    return redirect ('/account')
+
+# ---- Farm ----
+@app.route('/farm')
+def farm():
+    return render_template('farm.html')
+
+# ---- Dojo ----
+@app.route('/dojo')
+def dojo():
+    return render_template('dojo.html')
